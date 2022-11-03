1a9ecdf246341e0be904c30ab4580fe52bf9880f
Adam <azreow@gmail.com>
2022-11-03 10:04:04 -0400

    Added Comments and Update Outlier Methodology
    The comments add context to the code blocks just in case they seem confusing and explain methodology of win condition in the MontyHallGame function definition. 
    The Outlier methodology now uses the data range to calculate quartiles so that numbers other than 100 can be used.

--- Monty Hall.py
+++ Monty Hall.py
@@ -1,36 +1,48 @@
 import random
 
+#The actual game: 
 def MontyHallGame():
     winCount = 0
-    for i in range(1, 101):
+    for i in range(0, 100):
         doors = ['goat', 'goat', 'goat']
         doorNum = random.randint(0, 2)
         doors[doorNum] = 'car'
+        """
+        We check if a car is in both 2 or 3 because we are trying to find if the player switches every time from door 1 to door 2 or 3, what the odds of winning is. 
+        Since we know that we are going to pick, 2 or 3 - whichever door that the host doesn't open - then we know that if the car is behind either of the two doors, we will win. 
+        """
         if doors[1] == 'car' or doors [2] == 'car':
             winCount += 1
     return winCount
 
+#playing the game 10,000 times and sorting the total win counts in order from least to greatest
 numWinList = []
-for i in range(0, 100):
+dataRange = 10000
+for i in range(0, dataRange):
     numWinList.append(MontyHallGame())
 numWinList.sort()
 
-IQR = numWinList[74] - numWinList[24]
+#calculate the IQR and determine what would be an outlier.
+thirdQuartile = int(dataRange - dataRange / 4)
+firstQuartile = int(dataRange / 4)
+IQR = numWinList[thirdQuartile] - numWinList[firstQuartile]
 outliers = IQR * 1.5
-bottomOutlier = numWinList[24] - outliers
-topOutlier = numWinList[74] + outliers
+topOutlier = numWinList[thirdQuartile] + outliers
+bottomOutlier = numWinList[firstQuartile] - outliers
 
+#calculate how many outliers exist if any.
 overOutliers = 0
 underOutliers = 0
-
 for num in numWinList:
     if num > topOutlier:
         overOutliers += 1
     elif num < bottomOutlier:
         underOutliers += 1
 
+#calculate the average amount of wins which is roughly equivalent to the probability of winning by switching doors. 
 average = sum(numWinList) / len(numWinList)
 
+#output results
 print('Bottom Outlier Number = ' + str(bottomOutlier))
 print('Top Outlier Number = ' + str(topOutlier))
 print('Number of outliers at the top: ' + str(overOutliers))
